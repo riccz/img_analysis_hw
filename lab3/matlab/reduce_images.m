e52c04348ce575d1e3560478eaf4bbc5cf8443ba
Riccardo Zanol <ricc@zanol.eu>
2016-04-08 15:48:01 +0200

    DNG writing does not work

--- lab3/matlab/reduce_images.m
+++ lab3/matlab/reduce_images.m
@@ -9,3 +9,15 @@ for i=1:length(test_imgs)
     center_img = img(floor(W/2)-9:floor(W/2)+10, floor(H/2)-9:floor(H/2)+10, :);
     imwrite(center_img, dstname);
 end
+% 
+% dng_imgs = {'DEI_park.dng' 'macbeth_gray.dng' 'poster.dng' ...
+%     'students2.dng' 'macbeth_color.dng' 'map.dng' ...
+%     'students1.dng' 'students3.dng'};
+% for i=1:length(dng_imgs)
+%     fname = ['../images/' dng_imgs{i}];
+%     dstname = ['../images/small_' dng_imgs{i}]
+%     img = imread(fname);
+%     [W, H, ~] = size(img);
+%     center_img = img(floor(W/2)-9:floor(W/2)+10, floor(H/2)-9:floor(H/2)+10, :);
+%     imwrite(center_img, dstname);
+% end
