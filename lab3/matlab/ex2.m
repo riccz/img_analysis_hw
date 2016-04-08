1ce1b1889bb7d641e6ceec35e33ee9919da14810
Riccardo Zanol <ricc@zanol.eu>
2016-04-08 15:56:35 +0200

    Part 2 done

---
+++ lab3/matlab/ex2.m
@@ -0,0 +1,26 @@
+close all; clear all; clc;
+
+dng_imgs = {'DEI_park.dng' 'macbeth_gray.dng' 'poster.dng' ...
+    'students2.dng' 'macbeth_color.dng' 'map.dng' ...
+    'students1.dng' 'students3.dng'};
+
+for i=1:length(dng_imgs)
+    fname = ['../images/' dng_imgs{i}];
+    bayer = read_dng(fname);
+    
+    % Convert rggb -> grbg (+1 column at the beginning and end)
+    [W, H, ~] = size(bayer);
+    bayer = [zeros(W, 1), bayer, zeros(W, 1)];
+    
+    bayer = uint8(bayer .* 255); % Convert to uint8 to use the same code
+    demosaic_img = demosaic_bilinear(bayer);
+    matlab_demosaic_img = double(demosaic(bayer, 'grbg')) ./ 255;
+    
+    % Remove the extra columns
+    demosaic_img = demosaic_img(:, 2:H+1, :);
+    matlab_demosaic_img = matlab_demosaic_img(:, 2:H+1, :);
+    
+    imwrite(demosaic_img, ['demosaic_' dng_imgs{i} '.tiff']);
+    imwrite(matlab_demosaic_img, ['matlab_demosaic_' dng_imgs{i} '.tiff']);
+    fprintf([dng_imgs{i} ' OK\n']);
+end
