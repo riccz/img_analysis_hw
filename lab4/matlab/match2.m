e56aaa42317c84441adaecdb031ffbb42a370e9b
Riccardo Zanol <ricc@zanol.eu>
2016-05-03 21:44:59 +0200

    Don't always show the matches

--- lab4/matlab/match2.m
+++ lab4/matlab/match2.m
@@ -8,7 +8,10 @@
 %
 % Example: match('scene.pgm','book.pgm');
 
-function [match, num] = match2(im1, des1, loc1, im2, des2, loc2, threshold)
+function [match, num] = match2(im1, des1, loc1, im2, des2, loc2, threshold, show)
+if nargin < 8
+    show = false;
+end
 
 % Find SIFT keypoints for each image
 %[im1, des1, loc1] = sift(image1);
@@ -36,26 +39,23 @@ for i = 1 : size(des1,1)
       match(i) = 0;
    end
 end
-
-% Create a new image showing the two images side by side.
-im3 = appendimages(im1,im2);
-
-% Show a figure with lines joining the accepted matches.
-figure('Position', [100 100 size(im3,2) size(im3,1)]);
-colormap('gray');
-imagesc(im3);
-hold on;
-cols1 = size(im1,2);
-for i = 1: size(des1,1)
-  if (match(i) > 0)
-    line([loc1(i,2) loc2(match(i),2)+cols1], ...
-         [loc1(i,1) loc2(match(i),1)], 'Color', 'c');
-  end
+if show
+    % Create a new image showing the two images side by side.
+    im3 = appendimages(im1,im2);
+    
+    % Show a figure with lines joining the accepted matches.
+    figure('Position', [100 100 size(im3,2) size(im3,1)]);
+    colormap('gray');
+    imagesc(im3);
+    hold on;
+    cols1 = size(im1,2);
+    for i = 1: size(des1,1)
+        if (match(i) > 0)
+            line([loc1(i,2) loc2(match(i),2)+cols1], ...
+                [loc1(i,1) loc2(match(i),1)], 'Color', 'c');
+        end
+    end
+    hold off;
 end
-hold off;
 num = sum(match > 0);
 fprintf('Found %d matches.\n', num);
-
-
-
-
