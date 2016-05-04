65fd61b6a176fcb82615a40e864d88a7dc3a2b7a
Riccardo Zanol <ricc@zanol.eu>
2016-05-04 16:32:23 +0200

    Finish lab4

--- lab4/matlab/lab4.m
+++ lab4/matlab/lab4.m
@@ -5,6 +5,7 @@ half_fov = 33;
 imgs_dir = '../lab_22_4_16_manual/';
 cil_dir = './cil_imgs/';
 mkdir(cil_dir);
+% Convert in cilindrical coords
 for i=1:12
     fname = [imgs_dir 'i' num2str(i) '.bmp'];
     img = imread(fname);
@@ -12,18 +13,31 @@ for i=1:12
     imwrite(cil_img, [cil_dir 'c' num2str(i) '.bmp']);
 end
 
-last_fname = [cil_dir 'c1.bmp'];
-[last_img, last_descr, last_loc] = sift(last_fname);
-total_img = last_img;
-for i=2:12
-    fname = [cil_dir 'c' num2str(i) '.bmp'];
-    [img, descr, loc] = sift(fname);    
-    [match, num] = match2(last_img, last_descr, last_loc, img, descr, loc, 0.8);    
-    [deltax, deltay, compat_count] = ransac_translation(last_loc, loc, match, 50, 3, 3);
-    total_img = [total_img, img(:,round(deltax)+1:size(img,2))];
-    last_img = img; last_descr = descr; last_loc = loc;
+% Match the SIFT features
+match_thresh = 0.7;
+ransac_n = 100;
+ransac_thresh_x = 20;
+ransac_thresh_y = ransac_thresh_x;
+deltas = [];
+for i=1:11
+    fname1 = [cil_dir 'c' num2str(i) '.bmp'];
+    fname2 = [cil_dir 'c' num2str(i+1) '.bmp'];
+    
+    [img1, descr1, loc1] = sift(fname1);
+    [img2, descr2, loc2] = sift(fname2);
+    
+    [match, num] = match2(img1, descr1, loc1, img2, descr2, loc2, match_thresh);
+    
+    [deltax, deltay, compat_count] = ransac_translation(loc1, loc2, match, ransac_n, ransac_thresh_x, ransac_thresh_y);
+    deltas(i+1,:) = [deltax, deltay];
 end
 
-figure;
-imshow(total_img);
-imwrite(total_img, 'panoramic_img.bmp');
+% Merge the images
+total_image = imread('cil_imgs/c1.bmp');
+for i=2:12
+    half_deltax = round(deltas(i,1) / 2);
+    cropped_total = imcrop(total_image, [0, 0, size(total_image, 2) + half_deltax, size(total_image, 1)]);
+    cropped_img = imcrop(imread(['cil_imgs/c' num2str(i) '.bmp']), [-half_deltax, 0, Inf, Inf]);
+    total_image = appendimages(cropped_total, cropped_img);
+end
+imwrite(total_image, 'panoramic_img.bmp');
